66329559b92fb3e5201e48c7e660820788c76e9e
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-02-27 21:28:07 +0500

    Adds Image and Price Input Fields in AddNewCourse

--- src/container/AddsCourse.js
+++ src/container/AddsCourse.js
@@ -4,12 +4,13 @@ import { addCourse } from '../redux/courses/coursesAction';
 
 const AddsCourse = () => {
   const dispatch = useDispatch();
-  const author = useSelector(state => state.user.user.username);
-  console.log(author);
+  const author = useSelector(state => state.user.user.user_id);
   const [title, setTitle] = useState('');
   const [details, setDetails] = useState('');
   const [requirements, setRequirements] = useState('');
   const [difficulty, setDifficulty] = useState('');
+  const [image, setImage] = useState('');
+  const [price, setPrice] = useState('');
 
   const handleChange = e => {
     if (e.target.name === 'title') {
@@ -20,10 +21,10 @@ const AddsCourse = () => {
       setRequirements(e.target.value);
     } else if (e.target.name === 'difficulty') {
       setDifficulty(e.target.value);
-    } else if (e.target.name === 'title') {
-      setTitle(e.target.value);
-    } else if (e.target.name === 'title') {
-      setTitle(e.target.value);
+    } else if (e.target.name === 'image') {
+      setImage(e.target.value);
+    } else if (e.target.name === 'price') {
+      setPrice(e.target.value);
     }
   };
 
@@ -34,7 +35,10 @@ const AddsCourse = () => {
       course_author: author,
       course_requirements: requirements,
       course_difficulty: difficulty,
+      course_image: image,
+      course_price: price,
     };
+    console.log(newCourse);
     dispatch(addCourse(newCourse));
   };
   return (
@@ -47,6 +51,8 @@ const AddsCourse = () => {
         <option value="Intermediate">Intermediate</option>
         <option value="Advanced">Advanced</option>
       </select>
+      <input type="text" placeholder="Paste Course Image Link" name="image" onChange={e => handleChange(e)} />
+      <input type="text" placeholder="Price of this course" name="price" onChange={e => handleChange(e)} />
       <input type="submit" value="SAVE" onClick={() => handleClick()} />
     </div>
   );
