f119c71e4c03da2547dbd7a289c4d80eaace8f50
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-02-28 20:56:15 +0500

    Adds All Author Courses

--- src/container/AuthorCourses.js
+++ src/container/AuthorCourses.js
@@ -1,9 +1,30 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { useSelector } from 'react-redux';
+// eslint-disable-next-line no-unused-vars
+import Courses from '../component/Courses';
 
-const AuthorCourses = () => (
-  <div>
-    Hey From ALl Author COurses
-  </div>
-);
+const AuthorCourses = () => {
+  const allCourses = useSelector(state => state.course.courses);
+  // eslint-disable-next-line no-unused-vars
+  const authorCourses = allCourses.filter(course => course.author === 2)
+    // eslint-disable-next-line no-unused-vars
+    .map(courseData => (
+      <Courses
+        key={courseData.course_id}
+        uniqueKey={courseData.course_id}
+        title={courseData.title}
+        total={allCourses.length}
+        index={allCourses.indexOf(courseData) + 1}
+        image={courseData.image}
+        price={courseData.price}
+      />
+    ));
+  return (
+    <div>
+      {authorCourses}
+    </div>
+  );
+};
 
 export default AuthorCourses;
--- src/container/AuthorPanelContainer.js
+++ src/container/AuthorPanelContainer.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter,
   Switch,
   Route,
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import AuthorPanelNavbar from '../component/AuthorPanelNavbar';
 import AddsCourse from './AddsCourse';
 import AuthorCourses from './AuthorCourses';
 import LogoutContainer from './LogoutContainer';
+import { fetchCourse } from '../redux/courses/coursesAction';
 
-const AuthorPanelContainer = () => (
-  <BrowserRouter>
-    <AuthorPanelNavbar />
-    <Switch>
-      <Route path="/addCourse" component={AddsCourse} exact />
-      <Route path="/authorCourses" component={AuthorCourses} exact />
-      <Route path="/logout" component={LogoutContainer} exact />
-    </Switch>
-  </BrowserRouter>
-);
+const AuthorPanelContainer = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCourse());
+  });
+  return (
+    <BrowserRouter>
+      <AuthorPanelNavbar />
+      <Switch>
+        <Route path="/addCourse" component={AddsCourse} exact />
+        <Route path="/authorCourses" component={AuthorCourses} exact />
+        <Route path="/logout" component={LogoutContainer} exact />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default AuthorPanelContainer;
