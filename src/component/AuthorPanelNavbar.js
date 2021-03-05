891e06275b164ba7bd3a9d589cb3b04259d88cea
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-03-05 16:49:12 +0500

    Removes Extra line

--- src/component/AuthorPanelNavbar.js
+++ src/component/AuthorPanelNavbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 const AuthorPanelNavbar = () => (
   <div className="authorNavbar">
     <Link to="/">
--- src/tests/empty.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { render, screen } from '@testing-library/react';
-
-describe('Test Suit', () => {
-  it('should.....', () => {
-  });
-});
